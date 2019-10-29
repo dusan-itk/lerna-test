5ed06bbdf069b7d029405c29d7e8de55d7b60701
Dušan Milosavljević <mdusan989@gmail.com>
2019-10-29 19:27:11 +0100

    Add module and main app

---
+++ packages/module-1/lib/HelloWorld.js
@@ -0,0 +1,14 @@
+var _jsxFileName = "/Users/dusan/Code/ITK2.noindex/lerna-test/packages/module-1/src/HelloWorld.js";
+import React from 'react';
+export function HelloWorld() {
+  return React.createElement("p", {
+    style: {
+      color: 'blue'
+    },
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4
+    },
+    __self: this
+  }, "Hello world from module 1");
+}
---
+++ packages/module-1/src/HelloWorld.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function HelloWorld() {
+  return <p style={{color: 'blue'}}>Hello world from module 1</p>;
+}
